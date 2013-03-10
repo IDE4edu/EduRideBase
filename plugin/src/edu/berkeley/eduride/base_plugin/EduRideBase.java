9f284fe0690d774494914451a5549a9e4582a120
Derek <Derek@crayon-pc>
2013-03-10 11:57:06 -0700

    Copied/added stuff to EduRideBase activator class

--- plugin/src/edu/berkeley/eduride/base_plugin/EduRideBase.java
+++ plugin/src/edu/berkeley/eduride/base_plugin/EduRideBase.java
@@ -1,11 +1,14 @@
 package edu.berkeley.eduride.base_plugin;
 
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class EduRideBase implements BundleActivator {
 
 	private static BundleContext context;
+	private static IEclipsePreferences prefs;
 
 	static BundleContext getContext() {
 		return context;
@@ -17,6 +20,7 @@ public class EduRideBase implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		EduRideBase.context = bundleContext;
+		prefs = InstanceScope.INSTANCE.getNode("EduRideBasePlugin");
 	}
 
 	/*
@@ -25,6 +29,38 @@ public class EduRideBase implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		EduRideBase.context = null;
+	}
+	
+	public static String whoami() {
+		return prefs.get("username", null);
+	}
+	
+	public static long hashID() {
+		return prefs.getLong("authHash", -1);
+	}
+
+	public static long workspaceID() {
+		return prefs.getLong("workspaceID", generateWsID());
+	}
+
+	private static long generateWsID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	private int generateAuthHash() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	private boolean authenticate(String username, String password) {
+		if (true /*something(username, password) */) {
+			int authHash = generateAuthHash();
+			prefs.put("username", username);
+			prefs.putInt("authHash", authHash);
+			return true;
+		}
+		return false;
 	}
 
 }
