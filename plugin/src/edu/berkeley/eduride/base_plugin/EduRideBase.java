d951e714d3e00b97bfc532a2d20e96d033db34fe
Nathaniel Titterton <nate@berkeley.edu>
2013-04-12 15:50:47 -0700

    put workflow for getting username in comments, refactored a bit

--- plugin/src/edu/berkeley/eduride/base_plugin/EduRideBase.java
+++ plugin/src/edu/berkeley/eduride/base_plugin/EduRideBase.java
@@ -28,7 +28,7 @@ public class EduRideBase extends AbstractUIPlugin implements IStartup {
 	
 	public static final String PLUGIN_ID = "EduRideBase";
 	public static final String DEFAULT_DOMAIN = "eduride.berkeley.edu";
-	public static final String guestUserName = "Guest";
+	public static final String GUEST_USER_NAME = "Guest";
 	// The shared instance
 	private static EduRideBase plugin = null;
 	private static UUID workspaceID = null;	
@@ -74,25 +74,85 @@ public class EduRideBase extends AbstractUIPlugin implements IStartup {
 	}
 	
 	/*
-	 * Returns null iff not logged in
-	 * @return
+	 * If username "U" has been chosen by user 
+	 *     (1) return "U" if they also have a *valid* authtoken
+	 *     (2) prompt login dialog if invalid authtoken (empty or expired)
+	 *         goto (1) if successfully authenticated with a username
+	 *         goto (3) if cancel login dialog or are offline
+	 *         goto (4) if they choose to remain a guest
+	 *     (3) return "U (?)" -- they choose this name but didn't/couldn't authenticate  
+	 * (4) Returns EduRideBase.guestUserName if they decided to stay a guest
+	 * Since they don't have a user name, prompt with the login dialog
+	 *     goto (1) if they successfully authenticate
+	 *     goto (4) if they choose to remain a guest
+	 *     (5) return null if they cancel or can't authenticate (e.g., offline) 
+	 * 
 	 */
 	public static String whoami() {
 		String username = prefs.get("username", null);
 		// perhaps push login dialog here (but not if they are looking at preferences)
 		// TODO need to distinguish between 'havent authenticated yet' and 'want to remain a guest'
 		if (username == null && loggedIn) {
-			username = guestUserName;
+			username = GUEST_USER_NAME;
 		}
 		return username;
+	}
+	
+	
+	public static String getUsername() {
+		return whoami();
+	}
+	
+	
+	/*
+	 * This differs from whoami() in that it won't ever prompt for login dialog in response.  (So, it gets a bunch more nulls)
+	 * 
+	 * If username "U" has been chosen by user 
+	 *     (1) return "U" if they also have a *valid* authtoken
+	 *     (2) return "U (?)" -- they choose this name but didn't/couldn't authenticate  
+	 * (3) Returns EduRideBase.guestUserName if they decided to stay a guest
+	 * (4) return null if they cancel or can't authenticate (e.g., offline) 
+	 */
+	public static String getCurrentUsername() {
+		String username = prefs.get("username", null);
+		if (username != null) {
+			// username isn't null
+			if (hasValidAuthToken()) {
+				return username;
+			} else {
+				return nonAuthenticatedUsername(username);
+			}
+		} else {
+			// username == null
+			boolean isGuest = prefs.getBoolean("isGuest", false);
+			if (isGuest) {
+				return GUEST_USER_NAME;
+			} else {
+				return null;
+			}
+		}
+	}
+	
+	
+	private static String nonAuthenticatedUsername(String username) {
+		return username + " (?)";
 	}
 	
 	public static String whereami() {
 		return prefs.get("domain", DEFAULT_DOMAIN);
 	}
 	
-	public static long hashID() {
-		return prefs.getLong("authHash", -1);
+	public static String getAuthToken() {
+		return prefs.get("authToken", null);
+	}
+	
+	
+	private static boolean hasValidAuthToken() {
+		// if offline, return false
+		// if authoken == null, return false
+		// check with server for current authtoken, return true iff server sez its valid
+		String authToken = getAuthToken();
+		return false;
 	}
 	
 	private static boolean isVerified(Object content) {
@@ -100,9 +160,9 @@ public class EduRideBase extends AbstractUIPlugin implements IStartup {
 		return true;
 	}
 	
-	public static boolean authenticate(String username, String password, String domain) {
+	private static boolean authenticate(String username, String password, String domain) {
 		boolean success;
-		if (username == guestUserName) {
+		if (username == GUEST_USER_NAME) {
 			success = true;
 		} else {
 //			try {
