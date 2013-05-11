a121ecf5d152d91283dab8eda7cf7ba82390a0d8
Nathaniel Titterton <nate@berkeley.edu>
2013-05-10 17:29:03 -0700

    moved json decode stuff here, and exporting it.  starting HTML parsing stuff here as well

---
+++ plugin/src/edu/berkeley/eduride/base_plugin/html/eduRideHtmlPage.java
@@ -0,0 +1,17 @@
+package edu.berkeley.eduride.base_plugin.html;
+
+import java.io.File;
+
+public class eduRideHtmlPage {
+
+	File file = null;
+	
+	
+	public eduRideHtmlPage(File file) {
+		this.file = file;
+	}
+	
+	
+	
+	
+}
