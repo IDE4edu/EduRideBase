c81bd24fabd40c886fd78fb76a90e2db1add2dd9
Nathaniel Titterton <nate@berkeley.edu>
2013-12-10 17:56:25 -0800

    added eduride exception types

---
+++ plugin/src/edu/berkeley/eduride/base_plugin/util/EduRideException.java
@@ -0,0 +1,11 @@
+package edu.berkeley.eduride.base_plugin.util;
+
+public class EduRideException extends Exception {
+
+	
+	public EduRideException(String msg) {
+		super(msg);
+	}
+	
+
+}
---
+++ plugin/src/edu/berkeley/eduride/base_plugin/util/ISAFormatException.java
@@ -0,0 +1,11 @@
+package edu.berkeley.eduride.base_plugin.util;
+
+public class ISAFormatException extends EduRideException {
+
+	public ISAFormatException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
