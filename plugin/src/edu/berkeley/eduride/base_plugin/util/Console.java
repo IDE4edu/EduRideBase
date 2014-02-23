53185dab28414de5e94a2949ec95ba0983528172
Nathaniel Titterton <nate@berkeley.edu>
2014-02-22 23:45:00 -0800

    eduride console, so users can see our awesome console messages

--- plugin/src/edu/berkeley/eduride/base_plugin/util/Console.java
+++ plugin/src/edu/berkeley/eduride/base_plugin/util/Console.java
@@ -1,17 +1,28 @@
 package edu.berkeley.eduride.base_plugin.util;
 
+import javax.swing.plaf.basic.BasicLookAndFeel;
+
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleFactory;
+import org.eclipse.ui.console.IConsoleManager;
 import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 import edu.berkeley.eduride.base_plugin.EduRideBase;
 
 
 
-public class Console {
+public class Console implements IConsoleFactory {
 
 	public static void msg (String msg) {
 		System.out.println(msg);
+		out.println(msg);
 	}
 	
 	public static void msg (Exception e) {
@@ -20,6 +31,7 @@ public class Console {
 	
 	public static void err (String msg) {
 		System.err.println(msg);
+		err.println(msg);
 	}
 	
 	public static void err (Exception e) {
@@ -30,19 +42,53 @@ public class Console {
 	// TODO this should throw an exception and put an entry into the problem view, yo
 	public static void isaErr (String msg) {
 		err(msg);
+		
 	}
 	
 	/////////////
 	
+	static private final String CONSOLE_NAME = "EduRide Console";
+	static private Device device = Display.getCurrent();
+	static private final Color RED = new Color(device, 255, 0, 0);
+	static private final Color BLACK = new Color(device, 0, 0, 0);
+	
+	static private MessageConsole console = null;
+	static private MessageConsoleStream out;
+	static private MessageConsoleStream err;
+	
+	static {
+		new Console();
+	}
+	
+	
 	public Console() {
-		ImageDescriptor imgDesc = EduRideBase.getImageDescriptor(EduRideBase.CAR_RED_IMAGE);
-		fMessageConsole = new MessageConsole("EduRide Console", imgDesc);
+		if (console == null) {
+			ImageDescriptor imgDesc = EduRideBase
+					.getImageDescriptor(EduRideBase.CAR_RED_IMAGE);
+			console = new MessageConsole(CONSOLE_NAME, imgDesc);
+			out = console.newMessageStream();
+			out.setColor(BLACK);
+			err = console.newMessageStream();
+			err.setColor(RED);
+
+			ConsolePlugin plugin = ConsolePlugin.getDefault();
+			IConsoleManager conMan = plugin.getConsoleManager();
+			conMan.addConsoles(new IConsole[]{console});
+		}
+		
+	}
+
+
+	
+	@Override
+	public void openConsole() {
+		console.activate();
+
 	}
 	
 	
 	
-	
-	MessageConsole fMessageConsole = null;
+
 	
 	
 	
